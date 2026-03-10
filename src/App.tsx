27903b00193be6a70657163d2bd34dd7f45cd6ab
m.prokhnovsky <mike0adler@gmail.com>
2026-03-10 17:40:42 +0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-indigo-600">
+        Tailwind v4 работает 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
